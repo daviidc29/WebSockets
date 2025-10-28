82759e9768cca37f1a0ae4e2092c95e964f01d62
David Castro <david.csierra@mail.escuelaing.edu.co>
2025-10-27 21:46:50 -0500

    final

--- src/main/java/edu/eci/arsw/WebSokets/configuration/WSConfigurator.java
+++ src/main/java/edu/eci/arsw/WebSokets/configuration/WSConfigurator.java
@@ -2,6 +2,7 @@ package edu.eci.arsw.WebSokets.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
@@ -9,15 +10,9 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @EnableScheduling
 public class WSConfigurator {
     
-    @Bean
+    @Bean   
+    @Profile("!test") // no se cargará en los tests
     public ServerEndpointExporter serverEndpointExporter() {
-        try {
-            Class.forName("jakarta.websocket.server.ServerContainer");
-            return new ServerEndpointExporter();
-        } catch (ClassNotFoundException e) {
-            // No hay servidor web en el entorno de test
-            return null;
-        }
+        return new ServerEndpointExporter();
     }
-
 }
--- src/test/java/edu/eci/arsw/WebSokets/WebSoketsApplicationTests.java
+++ src/test/java/edu/eci/arsw/WebSokets/WebSoketsApplicationTests.java
@@ -2,8 +2,11 @@ package edu.eci.arsw.WebSokets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class WebSoketsApplicationTests {
 
 	@Test
